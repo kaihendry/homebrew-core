ae0de3283a5d78e90b8baf4757d6d5fc261e6857
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-19 14:15:12 +0000

    chronograf 1.11.0

--- Formula/c/chronograf.rb
+++ Formula/c/chronograf.rb
@@ -1,8 +1,8 @@
 class Chronograf < Formula
   desc "Open source monitoring and visualization UI for the TICK stack"
   homepage "https://docs.influxdata.com/chronograf/latest/"
-  url "https://github.com/influxdata/chronograf/archive/refs/tags/1.10.9.tar.gz"
-  sha256 "15c7cc80cc5e1e2d6ef33bffce1b6a26729f45b80453714f424eec03974f7e14"
+  url "https://github.com/influxdata/chronograf/archive/refs/tags/1.11.0.tar.gz"
+  sha256 "11e1228f3b193d7b9cafad6ff4903f0f920a03432c5045ee620c105e361ced10"
   license "AGPL-3.0-or-later"
   head "https://github.com/influxdata/chronograf.git", branch: "master"
 
@@ -19,31 +19,11 @@ class Chronograf < Formula
   # Avoid C++20 from Node 23+ until "@parcel/watcher >= 2.2.0" for "node-addon-api >= 4"
   # Issue ref: https://github.com/nodejs/node-addon-api/issues/1007
   depends_on "node@22" => :build
-  depends_on "python-setuptools" => :build
   depends_on "yarn" => :build
   depends_on "influxdb"
   depends_on "kapacitor"
 
   def install
-    # Check if workarounds can be removed
-    if build.stable?
-      yarn_lock = File.read("yarn.lock")
-
-      # distutils usage removed in https://github.com/nodejs/node-gyp/commit/707927cd579205ef2b4b17e61c1cce24c056b452
-      node_gyp_version = Version.new(yarn_lock[%r{/node-gyp[._-]v?(\d+(?:\.\d+)+)\.t}i, 1])
-      odie "Remove `python-setuptools` dependency!" if node_gyp_version >= 10
-
-      # node 22 (V8 12) fix in https://github.com/nodejs/nan/commit/1b630ddb3412cde35b64513662b440f9fd71e1ff
-      nan_version = Version.new(yarn_lock[%r{/nan[._-]v?(\d+(?:\.\d+)+)\.t}i, 1])
-      odie "Remove `nan` resolution workaround!" if nan_version >= "2.19.0"
-    end
-
-    # Workaround to build with newer node until `nan` dependency is updated
-    # https://github.com/influxdata/chronograf/issues/6040
-    package_json = JSON.parse(File.read("package.json"))
-    (package_json["resolutions"] ||= {})["nan"] = "2.23.0"
-    File.write("package.json", JSON.pretty_generate(package_json))
-
     ENV["npm_config_build_from_source"] = "true"
     ENV.deparallelize
     system "make"
