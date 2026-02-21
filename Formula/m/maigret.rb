345d3258f1a07797f673341779c4a402ac3eb53e
Michael Cho <michael@michaelcho.dev>
2026-02-20 21:23:20 -0500

    maigret: use `zlib-ng-compat` on Linux

--- Formula/m/maigret.rb
+++ Formula/m/maigret.rb
@@ -27,7 +27,10 @@ class Maigret < Formula
 
   uses_from_macos "libxml2", since: :ventura
   uses_from_macos "libxslt"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   pypi_packages exclude_packages: ["certifi", "cryptography", "pillow"]
 
